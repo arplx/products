e364df1d3100eca075c550e9e9025b330cca705b
arplx <93895162+arplx@users.noreply.github.com>
2024-01-16 18:37:52 +1200

    Update script.js

--- script.js
+++ script.js
@@ -7,12 +7,12 @@ fetch('products.json')
       const productElement = document.createElement('div');
       productElement.classList.add('product');
       productElement.innerHTML = `
-        <a class="product-tittle" href="${product.url}" target="_blank">
-          <h2>${product.name}</h2>
+      <img src="${product.image}" alt="${product.name}" style="width: 100%;height: 400px;object-fit: cover;object-position: top;display: block;">
+      <a class="product-tittle" href="${product.url}" target="_blank" style="padding: 0px;margin: 0px;text-decoration: none;">
+          <h2 style="margin: 0px;font-size: 15px;text-align: center;text-decoration: none;color: black;">${product.name}</h2>
         </a>
-        <p>${product.description}</p>
-        <img src="${product.image}" alt="${product.name}" />
-        <a class="buy-button" href="${product.url}" target="_blank">Buy on Amazon</a>
+      <p style="font-size: 11px;font-family: helvetica;padding: 0px;margin: 0px;margin-bottom: 10px;max-height: 97px;overflow: scroll;">${product.description}</p>
+      <a class="buy-button" href="${product.url}" target="_blank" style="width: auto;background: black;color: white;">BUY</a>
       `;
       productsContainer.appendChild(productElement);
     });
