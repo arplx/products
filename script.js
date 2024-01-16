0b292392674a2bc9bcd9d15513ce582085223ed8
arplx <93895162+arplx@users.noreply.github.com>
2024-01-16 20:17:12 +1200

    Update script.js

--- script.js
+++ script.js
@@ -11,9 +11,9 @@ fetch('products.json')
       <a class="product-tittle" href="${product.url}" target="_blank" style="padding: 0px;margin: 0px;text-decoration: none;">
           <h2 style="margin: 0px;font-size: 15px;text-align: center;text-decoration: none;color: black;">${product.name}</h2>
         </a>
-      <p style="font-size: 11px;font-family: helvetica;padding: 0px;margin: 0px;max-height: 97px;overflow: scroll;font-size: 18px;font-weight: 600;text-align: center;margin-top: 5px;margin-bottom: -5px;">${product.price}</p>
-      <a <button="" href="${product.url}" type="button" style="height: 45px;padding: 0px;background-color: #000;width: 300px;border-radius: 10px;border: 1px solid #2928e6;">
-  <img alt="Image description" style="height: 45px;padding-left: 85px;padding: 2;margin-left: 85px;" src="https://i.postimg.cc/SRwHB50H/Screenshot-2023-12-25-at-15-57-13-Aria-1.png">
+      <p style="font-size: 11px;font-family: helvetica;padding: 0px;margin: 0px;max-height: 97px;overflow: scroll;font-size: 18px;font-weight: 600;text-align: center;margin-top: 5px;margin-bottom: 5px;">${product.price}</p>
+      <a <button="" href="${product.url}" type="button" style="height: 45px;padding: 0px;background-color: #000;width: 300px;border-radius: 10px;width: 100%;">
+  <img alt="Image description" style="height: 45px;padding: 2;display: flex;width: 100%;object-fit: contain;background: #ff9902;border-radius: 10px;" src="https://www.catchfirecreative.com/wp-content/uploads/2018/02/amazon-on-the-brain-blog.png">
 </a>
       `;
       productsContainer.appendChild(productElement);
